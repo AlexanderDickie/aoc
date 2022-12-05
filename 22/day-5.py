255cdac14ab33c2b9027a17daad04e9801e87367
alexanderdickie <75994927+AlexanderDickie@users.noreply.github.com>
2022-12-05 10:22:27 +0000

    d5

---
+++ 22/day-5.py
@@ -0,0 +1,60 @@
+from request import get_input
+
+
+def part_1(inp):
+    top, bottom = inp[:-1].split('\n\n')
+    top = top.split('\n')[:-1]
+    bottom = bottom.split('\n')
+
+    stacks = []
+    for x in range(1, len(top[0]), 4):
+        stacks.append([])
+        for y in range(len(top)):
+            if top[y][x] != ' ':
+                stacks[-1].append(top[y][x])
+
+    for line in bottom:
+        line = line.split(' ')
+        a = int(line[1])
+        b = int(line[3])
+        c = int(line[5])
+
+        for i in range(a):
+            mv = stacks[b-1].pop(0)
+            stacks[c-1] = [mv] + stacks[c-1]
+
+    return "".join([stack[0] for stack in stacks])
+
+# 5. 30
+
+print(part_1(get_input(5, 0)))
+print(part_1(get_input(5, 1)))
+
+def part_2(inp):
+    top, bottom = inp[:-1].split('\n\n')
+    top = top.split('\n')[:-1]
+    bottom = bottom.split('\n')
+
+    stacks = []
+    for x in range(1, len(top[0]), 4):
+        stacks.append([])
+        for y in range(len(top)):
+            if top[y][x] != ' ':
+                stacks[-1].append(top[y][x])
+
+    for line in bottom:
+        line = line.split(' ')
+        a = int(line[1])
+        b = int(line[3])
+        c = int(line[5])
+
+        mv = []
+        for i in range(a):
+            mv.append(stacks[b-1].pop(0))
+        stacks[c-1] = mv + stacks[c-1]
+
+
+    return "".join([stack[0] for stack in stacks])
+
+print(part_2(get_input(5, 0)))
+print(part_2(get_input(5, 2)))
